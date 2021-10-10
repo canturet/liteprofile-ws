705cca1d33170b90a01a083e3e6a6d27b552e40d
canturet <turetcan@gmail.com>
2021-10-10 12:48:56 +0300

    user repository and role repository created

---
+++ src/main/java/com/liteprofile/ws/repository/RoleRepository.java
@@ -0,0 +1,15 @@
+package com.liteprofile.ws.repository;
+
+import com.liteprofile.ws.model.ERole;
+import com.liteprofile.ws.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByName(ERole name);
+
+}
---
+++ src/main/java/com/liteprofile/ws/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.liteprofile.ws.repository;
+
+import com.liteprofile.ws.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
+
+}
